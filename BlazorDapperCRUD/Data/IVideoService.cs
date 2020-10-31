8aeb3aba2d92f1a78598d04eb54a4c9fe411f31c
Javad Hamzeh <hamzehneisiany7741@saskpolytech.ca>
2020-10-31 15:56:28 -0600

    Add project files.

---
+++ BlazorDapperCRUD/Data/IVideoService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorDapperCRUD.Data
+{
+    public interface IVideoService
+    {
+        Task<bool> VideoInsert(Video video);
+        Task<IEnumerable<Video>> VideoList();
+
+        Task<Video> Video_GetOne(int Id);
+        Task<bool> VideoUpdate(Video video);
+        Task<bool> VideoDelete(int Id);
+    }
+}
